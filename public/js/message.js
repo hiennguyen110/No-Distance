9dec4b366d67fdff18de72ea63af57da585608cf
Hien Nguyen <hnguyen110@myseneca.ca>
2019-10-13 22:02:18 -0400

    Adding sharing location function into the app

--- public/js/message.js
+++ public/js/message.js
@@ -1,4 +1,18 @@
 const socket = io();
+const $message_container = document.querySelector("#message-input");
+const $new_message = document.querySelector('#new-message');
+const $sendMessageBtn = document.querySelector('#submit-message');
+const $locationField = document.querySelector("#share-location");
+const $sharingLocationBtn = document.querySelector("#current-location");
+
+
+socket.on("greetingMess", (message) => {
+    console.log(message);
+});
+
+socket.on("newUser", (message) => {
+    console.log(message);
+});
 
 socket.on("updatedMessage", (messages) => {
     let len = messages.length;
@@ -8,8 +22,40 @@ socket.on("updatedMessage", (messages) => {
     }
 });
 
-document.querySelector("#message-input").addEventListener("submit", (e) => {
+socket.on("iLocation", (latitude, longitude, link) => {
+    console.log(link);
+});
+
+$message_container.addEventListener("submit", (e) => {
     e.preventDefault();
-    let new_message = document.getElementById("new-message").value;
-    socket.emit("new_message", new_message);
+    $sendMessageBtn.setAttribute('disabled', 'disabled');
+    let new_message = e.target.elements.new_message.value;
+    socket.emit("new_message", new_message, (message) => {
+        $sendMessageBtn.removeAttribute('disabled');
+        $new_message.value = " ";
+        $new_message.focus();
+        // The second message is the callback from the server
+        console.log(message);
+    });
+});
+
+$locationField.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $sharingLocationBtn.setAttribute('disabled', 'disabled');
+    console.log("Activate Location Function");
+    if (!navigator.geolocation){
+        return alert("Geo location is not supported on your machine :[[[");
+    } else {
+        navigator.geolocation.getCurrentPosition((position) => {
+            socket.emit("user-location", {
+                latitude: position.coords.latitude,
+                longtitude: position.coords.longitude,
+                locationLink: `https://google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`,
+            }, (result_message) => {
+                $sharingLocationBtn.removeAttribute('disabled');
+                $new_message.focus();
+                console.log(result_message);
+            });
+        });
+    }
 });
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const path = require("path");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,13 +15,35 @@ app.use(express.static(publicDirPath));
 
 // This is the messages
 let messages = [];
-
+messages.push("Welcome to the town !!!");
 io.on("connection", (socket) => {
+    const filter = new Filter();
     console.log("Accepting new connection ...");
-    io.emit("updatedMessage", messages);
-    socket.on("new_message", (message) => {
-        messages.push(message);
-        console.log("Sending updated message back to the client ...");
+    
+    socket.emit("greetingMess", "Hey, welcome to the town :}}}");
+    socket.broadcast.emit("newUser", "A new user has joined the town :]]]");
+
+    socket.on("new_message", (message, callback) => {
+        if (filter.isProfane(message)){
+            return callback("This message contains some bad words ...");
+        } else {
+            messages.push(message);
+            console.log("Sending updated message back to the client ...");
+            io.emit("updatedMessage", messages);
+            callback("");
+        }
+    });
+
+    socket.on("user-location", (location, callback) => {
+        console.log("New location recieved from the user ...");
+        console.log("Latitude: ", location.latitude);
+        console.log("Longtitude: ", location.longtitude);
+        io.emit("iLocation", location.latitude, location.longtitude, location.locationLink);
+        return callback("Location has been shared to the public ...");
+    });
+
+    socket.on("disconnect", () => {
+        messages.push("A friend has left the town :[[[");
         io.emit("updatedMessage", messages);
     });
 });
@@ -28,3 +51,10 @@ io.on("connection", (socket) => {
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// The server will recieve the message from a client
+// The message will be checked if there is some bad words in it or not
+// The message will be returned back to the client using callback function
+
+// On the client side
+// In the sending function, the client will get a message using the callback
